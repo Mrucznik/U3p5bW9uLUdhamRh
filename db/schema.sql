b3aaab8f7be3020d789ec66f67b80923f3d6ab68
Mrucznik <mrucznix@gmail.com>
2020-10-01 12:13:24 +0200

    Dockerfiles, SQL schema

---
+++ db/schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE `urls`
+(
+    `id`          int(11) AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    `address`        varchar(2048)            NOT NULL,
+    `interval` int(11) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+CREATE TABLE `url_history`
+(
+    `id`          int(11) AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    `url_id`          int(11) FOREIGN KEY,
+    `response` TEXT NULL,
+    `duration` DOUBLE,
+    `created_at` TIMESTAMP,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY(`url_id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
