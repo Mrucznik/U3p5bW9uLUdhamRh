c0627aa62679a7fe67ec9881e69319ae879ee36d
Mrucznik <mrucznix@gmail.com>
2020-10-01 13:34:40 +0200

    Correct db schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `urls`
 (
-    `id`          int(11) AUTO_INCREMENT NOT NULL,
-    `address`        varchar(2048)            NOT NULL,
+    `id` int(11) AUTO_INCREMENT NOT NULL,
+    `address` varchar(2048) NOT NULL,
     `interval` int(11) NOT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
@@ -9,12 +9,12 @@ CREATE TABLE `urls`
 
 CREATE TABLE `url_history`
 (
-    `id`          int(11) AUTO_INCREMENT NOT NULL,
-    `url_id`          int(11) FOREIGN KEY,
-    `response` TEXT NULL,
-    `duration` DOUBLE,
-    `created_at` TIMESTAMP,
+    `id` int(11) AUTO_INCREMENT NOT NULL,
+    `url_id` int(11) NOT NULL,
+    `response` TEXT,
+    `duration` DOUBLE NOT NULL,
+    `created_at` TIMESTAMP NOT NULL,
     PRIMARY KEY (`id`),
-    FOREIGN KEY(`url_id`) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY(`url_id`) REFERENCES urls(id)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
